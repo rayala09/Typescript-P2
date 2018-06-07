c8fe6df9a23eec14bb1935a9ff7a82d2eec22bbe
Moises <moyayala12@gmail.com>
2018-06-07 11:35:31 -0700

    Interface practices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,23 @@ import { Component, OnInit } from '@angular/core';
 interface Person {
   firstName: string;
   lastName: string;
+  sayHi(): string;
+}
 
-
-
-
-  sayHi(s: string): string;
-
-
-
-
-
-
+/** class Customer {
+ * constructor() {}
+ * sayHi(name): string {
+ *  console.log('Hiii'+ name);
+ *  return 'Hii' +name;
+ * }
 
 }
+
+ class Employee {
+
+ }
+
+*/
 
 
 @Component({
@@ -27,12 +31,25 @@ export class AppComponent implements OnInit {
   title = 'app';
 
   ngOnInit() {
-    this.interfaceTest({firstName: 'Ray', lastName: 'Ayala', sayHi() });
+    const customer: Person = {
+      firstName: 'Ray',
+      lastName: 'Ayala',
+      sayHi: (): string => {
+        return 'Hi there';
+      }
+    };
+
+    const employee: Person = {
+      firstName: 'Josh',
+      lastName: 'Whitter',
+      sayHi: (): string => {
+        return 'Hello';
+
+      }
+    };
+
+    console.log(employee.sayHi());
+    console.log(customer.sayHi());
   }
 
-  interfaceTest(x: Person) {
-    console.log(`${x.firstName}  ${x.lastName}`);
-
-
   }
-}
